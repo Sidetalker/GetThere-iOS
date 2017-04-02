45d0eabd0cbda59065e83ebc4e9bd54e3653f69c
Kevin Sullivan <kevin@sideapps.com>
2017-04-02 15:39:22 -0400

    Initial Commit

---
+++ GetThere/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GetThere
+//
+//  Created by Kevin Sullivan on 4/2/17.
+//  Copyright © 2017 SideApps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
